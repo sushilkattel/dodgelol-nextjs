ae61c12a0eac5a3a93e428b9a30697276a5b6d5e
sushilkattel <info@sushilkattel.com>
2022-07-27 00:52:00 -0500

    Fixed API calls to call server

--- src/apiUtils/apiUtils.tsx
+++ src/apiUtils/apiUtils.tsx
@@ -1,12 +1,12 @@
 export const summonerDataQuery = (summonerName?: string): string => {
-  return `http://localhost:3080/summonerDataQuery?user=${summonerName}`;
+  return `http://143.198.77.70:3080/summonerDataQuery?user=${summonerName}`;
 };
 export const summonerRankedQuery = (id?: string): string => {
-  return `http://localhost:3080/summonerRankedQuery?user=${id}`
+  return `http://143.198.77.70:3080/summonerRankedQuery?user=${id}`
 }
 export const summonerGameQuery = (gameId?: string): string => {
-  return `http://localhost:3080/summonerGameQuery?user=${gameId}`
+  return `http://143.198.77.70:3080/summonerGameQuery?user=${gameId}`
 }
 export const summonerMatchlistQuery = (puuid?: string): string => {
-  return `http://localhost:3080/summonerMatchlistQuery?user=${puuid}`
+  return `http://143.198.77.70:3080/summonerMatchlistQuery?user=${puuid}`
 }
